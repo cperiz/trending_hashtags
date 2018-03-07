41781f8b65178a356caffd734edc40d59965a494
Charith <charith.peris@gmail.com>
2018-03-07 12:35:25 +0530

    Split receiver_plot.py into two in order to plot both hash tags and replies

---
+++ receiver_plot_replies.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import pika
+from lib.plotter import PlotHist
+
+class Receiver():
+
+    def __init__(self):
+
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+        self.channel = self.connection.channel()
+        self.channel.queue_declare(queue='reply_feed')
+        self.channel.basic_consume(self.callback,
+                              queue='reply_feed',
+                              no_ack=True)
+        self.plot_hist = PlotHist('r')
+
+    def callback(self, ch, method, properties, body):
+        print(" [x] Received %r" % body)
+        if len(body) > 0:
+            body = body.decode('utf-8')
+            tup_list = [r.split("|::|") for r in body.split("|||")]
+            #: decode unicode and int
+            tup_list = [(i[0], int(i[1])) for i in tup_list]
+            self.plot_hist.plot(tup_list=tup_list)
+
+
+if __name__ == '__main__':
+
+    r = Receiver()
+    print(' [*] Waiting for tags. To exit press CTRL+C')
+    r.channel.start_consuming()
